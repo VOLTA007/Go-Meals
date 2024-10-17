fa85115a583761582ff7a7cc8dcc9a918929bc48
Volta <voltagaming2001@gmail.com>
2024-10-17 04:02:07 +0300

    Add initial project configuration and files

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      {/* <div className="flex justify-center items-center">
+        <h1 className="text-green-600">Go-Meals</h1>
+      </div> */}
+    </div>
+  );
+}
