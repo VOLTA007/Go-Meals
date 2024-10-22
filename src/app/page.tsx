f9245f8a4afc6dc38e2a956cad28fedd1f82875f
Volta <voltagaming2001@gmail.com>
2024-10-22 05:20:24 +0300

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
       {/* <div className="flex justify-center items-center">
         <h1 className="text-green-600">Go-Meals</h1>
       </div> */}
+      <h1>Salman 5awal</h1>
     </div>
   );
 }
